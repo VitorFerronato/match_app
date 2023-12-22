590ff5239776498f2e6ae97cdf43a641a7fecbfa
Vitor Eduardo <vitor.eduardo@predify.me>
2023-12-22 19:23:00 -0300

    feat: topbar

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-main>
+      <Top-navbar />
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import TopNavbar from './components/top-navbar.vue'
+
+export default {
+  components: { TopNavbar },
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+}
+</script>
+
---
+++ src/components/top-navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav class="nav-main">
+    <div class="flex-center">
+      <span class="font-bold text-white logo">LOGO</span>
+      <v-text-field
+        variant="outlined solo"
+        density="compact"
+        single-line
+        hide-details
+        class="ml-10 search"
+      >
+        <template v-slot:label>
+          <span class="text-white font-bold">Buscar</span>
+        </template>
+      </v-text-field>
+    </div>
+
+    <div class="square"></div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.nav-main {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-image: linear-gradient(to right, #5e62db, #222e6d);
+  padding: 10px 25px;
+
+  .logo {
+    font-family: var(--font-family-base);
+  }
+
+.search{
+    width: 250px;
+    background-color: rgba(20, 19, 19, 0.26);
+    border-radius: 8px;
+}
+  .square {
+    width: 30px;
+    height: 30px;
+    background-color: rgba(255, 255, 255, 0.08);
+    margin-right: 54px;
+    border-radius: 8px;
+  }
+}
+</style>
